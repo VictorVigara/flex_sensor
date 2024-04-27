85b74f8694c6f41b7d0057e1cd5de57504908ad0
VictorVigara <victor@meilirobots.com>
2024-04-27 18:15:46 +0200

    converting analog into Voltage for resistor testing

--- flex_sensor/flex_sensor_driver.py
+++ flex_sensor/flex_sensor_driver.py
@@ -12,7 +12,9 @@ class flexDriver(Node):
         self.timer_period = 0.01
 
         # Set-up parameters
-        self.n_sensors = 3
+        self.n_sensors = 1
+        self.VCC = 5 # Voltage at arduino 5V line
+        self.R_DIV = 82000
 
         # Connection parameters
         self.port = '/dev/ttyACM0'
@@ -20,6 +22,8 @@ class flexDriver(Node):
         
         # Initialize variables
         self.pin_list = []
+        self.v_max = 0
+        self.v_min = 9999
 
         self.pin_initialization()
 
@@ -34,8 +38,16 @@ class flexDriver(Node):
         '''Method executed at a fixed rate self.timer_period'''
 
         for i in range(self.n_sensors):
-            value = self.pin_list[i].read()
-            print(f"A{i}: {value}")
+            ADC_flex = self.pin_list[i].read()*1000
+            V_flex = ADC_flex * self.VCC / 1023
+
+            if V_flex > self.v_max: 
+                self.v_max = V_flex
+            if V_flex < self.v_min: 
+                self.v_min = V_flex
+            R_flex = self.R_DIV * (self.VCC / V_flex -1)
+            print(f"A{i} V: {V_flex}V / R:{R_flex} ohms")
+            print(f"V_max: {self.v_max} / V_min: {self.v_min}")
 
     def pin_initialization(self): 
         """Initialize which pins to read"""
